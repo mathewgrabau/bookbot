556af60905ba0422b8fb5807fd4e1f2f698029ec
Mathew Grabau <mgrabau@mathewgrabau.ca>
2024-06-04 02:33:36 -0500

    Added letter counts.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+def count_letters(book_text):
+    letters = {}
+    for w in book_text.split():
+        for l in w.lower():
+            if not l in letters:
+                letters[l] = 0
+            letters[l] += 1
+
+    return letters
+
+
+def main():
+    with open("books/frankenstein.txt") as book_file:
+        book_contents = book_file.read()
+        words = len(book_contents.split())
+        print(words)
+
+        letters = count_letters(book_contents)
+        print(letters)
+
+
+if __name__ == "__main__":
+    main()
+
