274c18f64e036eb06f05bb01aef0cf7493e4e0ba
Mathew Grabau <mgrabau@mathewgrabau.ca>
2024-06-04 02:43:50 -0500

    Report implemented.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def count_letters(book_text):
     letters = {}
     for w in book_text.split():
         for l in w.lower():
+            if not l.isalpha():
+                continue
             if not l in letters:
                 letters[l] = 0
             letters[l] += 1
@@ -9,14 +11,31 @@ def count_letters(book_text):
     return letters
 
 
+def sort_on(dict):
+    return dict["count"]
+
 def main():
-    with open("books/frankenstein.txt") as book_file:
+    fname = "books/frankenstein.txt"
+    with open(fname) as book_file:
         book_contents = book_file.read()
         words = len(book_contents.split())
-        print(words)
 
         letters = count_letters(book_contents)
-        print(letters)
+        letters_sorted = []
+        for l in letters:
+            letters_sorted.append({"count": letters[l], "letter": l})
+        letters_sorted.sort(reverse=True, key=sort_on)
+
+        print(f"--- Begin report of {fname} ---")
+        print(f"{words} words found in the document\n")
+
+        for letter_dict in letters_sorted:
+            letter = letter_dict["letter"]
+            letter_count = letter_dict["count"]
+            print(f"The '{letter}' character was found {letter_count} times")
+
+        print("--- End report ---")
+
 
 
 if __name__ == "__main__":
